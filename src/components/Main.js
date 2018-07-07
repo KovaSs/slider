65acc102b24a3b509887ec4ad5a4b94b21afbd22
Den Isaev <sr_relikt@mail.ru>
2018-07-07 21:39:45 +0300

    Второй день верстки

---
+++ src/components/Main.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
+
+const Repair = styled.div`
+		color: #ffffff;
+		font-weight: 700;
+		line-height: 50px;
+		font-size: 40px;
+
+		span {
+			display: block;
+			font-size: 24px;
+			line-height: 30px;
+		}
+`
+const LightText = styled.div`
+		color: #ffffff;
+		font-size: 18px;
+		font-weight: 300;
+		line-height: 28px;
+		margin-top: 54px;
+`
+const MainButton = styled.button`
+		width: 247px;
+		height: 67px;
+		background-color: #ffa14b;
+		border-radius: 30px;
+		color: #ffffff;
+		font-size: 18px;
+		font-weight: bold;
+		line-height: 24px;
+		margin-top: 30px;
+		border: none;
+
+		&:hover {
+			cursor:pointer;
+		}
+		&:active {
+			-moz-transform: translatey(3px); /* Для Firefox */ 
+  		-ms-transform: translatey(3px); /* Для IE */ 
+  		-webkit-transform: translatey(3px); /* Для Safari, Chrome, iOS */ 
+  		-o-transform:translatey(3px); /* Для Opera */ 
+  		transform: translatey(3px)
+		}
+`
+
+class Main extends Component {
+  render() {
+    return (
+           <Row>
+           	<Col lg={5}>
+           	<Repair>
+           		Качественный ремонт
+           		<span>iphone за 35 минут и гарантия 1 год</span>
+           	</Repair>
+           		<LightText>
+           			Оставьте заявку на бесплатную диагностику без очереди,
+           			и получите защитное стекло, стоимостью 1000 рублей,
+           			с установкой в подарок!
+           		</LightText>
+           		<MainButton>
+           			Отправить заявку!
+           		</MainButton>
+           	</Col>
+           	<Col lg={6} lgOffset={1}>
+           	
+           	</Col>
+           </Row>
+    );
+  }
+}
+
+
+
+export default Main
